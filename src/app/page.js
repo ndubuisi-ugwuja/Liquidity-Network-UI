645d60f73e21e7c8b47678a631a789b696d94c87
Ndubuisi <ndujiovita1@gmail.com>
2025-12-09 11:41:45 +0100

    fix x overflow

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { Header, Main, Footer } from "./components";
 
 export default function Home() {
     return (
-        <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen py-10 px-5 gap-16 sm:p-20 w-full max-w-full">
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen py-10 px-8 gap-16 sm:p-20 w-full max-w-full">
             <Header />
             <Main />
             <Footer />
