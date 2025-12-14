6ec182f329228420d3949aebdabd218273e0098e
Ndubuisi <ndujiovita1@gmail.com>
2025-12-14 11:48:32 +0100

    add github source code

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-            <a href="#">source code</a>
+            <a href="https://github.com/ndubuisi-ugwuja/DeFi-Liquidity-Network">source code</a>
             <a href="https://github.com/ndubuisi-ugwuja/Liquidity-Network-UI/blob/main/README.md">docs</a>
             <a href="#">etherscan</a>
         </footer>
